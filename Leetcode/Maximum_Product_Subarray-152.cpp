685d2ac82e395f7d106cb3c58ecfec40e6819376
Kalpana <KALPANAKATHAIT11@GMAIL.COM>
2022-06-09 11:54:49 +0530

    Leetcode Questions

---
+++ Leetcode/Maximum_Product_Subarray-152.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+ int maxProduct(vector<int>& nums)
+{
+   int n=nums.size();
+          
+}
+int main()
+{
+   vector<int>nums={2,3,-2,4};
+   cout<<maxProduct(nums)<<endl;
+}
