be473094d3607776a511c96199eaecc156cdc3d2
Kalpana <KALPANAKATHAIT11@GMAIL.COM>
2022-06-10 15:42:01 +0530

    modified maximum-product-subarray

--- Leetcode/Maximum_Product_Subarray-152.cpp
+++ Leetcode/Maximum_Product_Subarray-152.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
- int maxProduct(vector<int>& nums)
-{
-   int n=nums.size();
-          
-}
+int maxProduct(vector<int>& nums) 
+    {
+       int maxi=nums[0];
+       int mini=nums[0];
+    int ans=nums[0];
+       for(int i=1; i<nums.size(); i++)
+       {
+           int temp=maxi;
+           maxi=max({nums[i],mini*nums[i],maxi*nums[i]});
+           mini=min({nums[i],temp*nums[i],mini*nums[i]});
+           ans=max(maxi,ans);
+       }
+        return ans;
+    }
 int main()
 {
    vector<int>nums={2,3,-2,4};
