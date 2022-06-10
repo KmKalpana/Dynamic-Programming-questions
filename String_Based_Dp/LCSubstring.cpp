2620581770a4f886f84ac89072bf3361bad98254
Kalpana <KALPANAKATHAIT11@GMAIL.COM>
2022-06-10 10:01:12 +0530

    Memoization LCSubstring

--- String_Based_Dp/LCSubstring.cpp
+++ String_Based_Dp/LCSubstring.cpp
@@ -1,15 +1,36 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
+int LCSubstring(string &s, string &t, int n, int m, vector<vector<int>>&dp,int ans)
+{
+       if(n==0||m==0)
+       {
+       return ans;
+       }
+       //if(dp[n][m]!=-1)
+       // return dp[n][m];
+       int lcs=ans;
+       if(s[n-1] == t[m-1])
+       {
+           lcs=LCSubstring(s,t,n-1,m-1,dp,ans+1);
+       }
+       int lcs1=LCSubstring(s,t,n-1,m,dp,0);
+       int lcs2=LCSubstring(s,t,n,m-1,dp,0);
+       return max({lcs,lcs1,lcs2});
+
+}
 int main()
 {
     string s="abcde";
     string t="decab";
    int n=s.length();
   int m= t.length();
-    vector<vector<int> >dp(n+1,vector<int>(m+1,0));
+    vector<vector<int> >dp(n+1,vector<int>(m+1,-1));
     int ans=0;
-    for(int i=1; i<=n; i++)
+    //Memoization method.
+    ans=LCSubstring(s,t,n,m,dp,ans);
+    //Tabular Method
+   /* for(int i=1; i<=n; i++)
     {
         for(int j=1; j<=m; j++)
         {
@@ -21,6 +42,6 @@ int main()
             else
                dp[i][j]=0;
         }
-    }
+    }*/ 
    cout<<ans<<endl; 
     }
